1d38e190bb9ddb2347cf88aa29af9829adad09ad
kang yeon joo <rkdduswn0409@gmail.com>
2023-09-02 21:57:14 +0900

    feat: Chapter 2 <스프링부트에서 테스트코드 작성하기> 공부 - "hello" 반환하는 컨트롤러 작성 + 테스트 코드 작성 - HelloResponseDto 반환하는 컨트롤러 작성 + 테스트 코드 작성 - 롬복 라이브러리 설정

--- src/main/java/com/example/web_service/WebServiceApplication.java
+++ src/main/java/com/example/web_service/WebServiceApplication.java
@@ -2,6 +2,7 @@ package com.example.web_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class WebServiceApplication {
---
+++ src/test/java/com/example/web_service/web/HelloControllerTest.java
@@ -0,0 +1,39 @@
+package com.example.web_service.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+@WebMvcTest
+class HelloControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void hello가_리턴된다() throws Exception {
+        String hello = "hello";
+
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+    }
+
+    @Test
+    public void hello_dto가_리턴된다() throws Exception{
+
+        String name="hello";
+        int amount=1000;
+
+        mvc.perform(get("/hello/dto").param("name",name).param("amount",String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name",is(name)))
+                .andExpect(jsonPath("$.amount",is(amount)));
+
+    }
+}
